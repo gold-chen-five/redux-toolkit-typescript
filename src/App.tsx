70b206660b7e350dd2e37879780bb36237a70e5a
黃暐程 <kevin@Jens-MacBook-Air.local>
2023-02-01 10:53:19 +0800

    20220201

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { useAppSelector, useAppDispatch } from '../src/redux/hooks/hooks'
+import { increment } from '../src/redux/CountSlice'
 function App() {
+  const {value} = useAppSelector(state => state.count)
+  const dispatch = useAppDispatch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>{value}</h1>
+     <button onClick={() => dispatch(increment())}>change value</button>
     </div>
   );
 }
---
+++ src/redux/CountSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+interface CounterState {
+    value: number
+}
+
+const initialState: CounterState = {
+    value: 0
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    // `createSlice` will infer the state type from the `initialState` argument
+    initialState,
+    reducers: {
+      increment: (state) => {
+        state.value += 1
+      },
+      decrement: (state) => {
+        state.value -= 1
+      },
+      // Use the PayloadAction type to declare the contents of `action.payload`
+      incrementByAmount: (state, action: PayloadAction<number>) => {
+        state.value += action.payload
+      },
+    },
+})
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export default counterSlice.reducer
